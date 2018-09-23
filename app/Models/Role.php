a9813080519d0572ee6e1ca4e9c4ef6c577712ab
Willy Nugraha <willymiiee@gmail.com>
2018-09-23 18:21:41 +0800

    Role model & migration

---
+++ app/Models/Role.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Role extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'job_title_id',
+        'module_id',
+        'sub_module_id',
+        'list',
+        'detail',
+        'create',
+        'update',
+        'delete'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
