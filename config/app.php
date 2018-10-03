0d54c27bbacd238a70410df4baa20aca5957ac12
Willy Nugraha <willymiiee@gmail.com>
2018-10-03 16:13:42 +0800

    add intervention/image

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +227,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        // Custom aliases
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
