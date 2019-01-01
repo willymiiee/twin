bc6b52b2651cdd1960823ea4122ec6c5f9c66056
Willy Nugraha <willymiiee@gmail.com>
2019-01-01 14:54:31 +0800

    update Authorization logic on main.js

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import axios from 'axios'
 import BootstrapVue from 'bootstrap-vue'
 import { Circle8 } from 'vue-loading-spinner'
 
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
+axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
+if (localStorage.token) {
+  axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.token
+}
+
 Vue.use(BootstrapVue)
 Vue.component('circle8', Circle8)
-Vue.prototype.$http = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-  headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-})
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
