ea3050b02be48679b295f15f7422ad95aa067a1e
Willy Nugraha <willymiiee@gmail.com>
2019-01-01 12:17:56 +0800

    add view for dashboard

---
+++ src/views/dashboard.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <section class="dashboard">
+    <div class="row">
+      <div class="col-12 grid-margin">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title mb-4">Selamat datang, {{ this.name }}!</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="js">
+export default {
+  name: 'dashboard',
+  data() {
+    return {
+      name: null
+    }
+  },
+  mounted() {
+    this.name = localStorage.userName
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
